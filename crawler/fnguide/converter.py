292c88f4316eb6af99ac8a7d8e9ae632d238c01e
Sean Kim <vanillasky9@gmail.com>
2021-07-22 10:09:38 +0900

    제무자료 파일변환 실패시 DB에 저장하도록 수정

--- crawler/fnguide/converter.py
+++ crawler/fnguide/converter.py
@@ -13,11 +13,16 @@ from skogkatt.core import LoggerFactory
 logger = LoggerFactory.get_logger(__name__)
 
 
+def save_failed_ticker(failed_codes):
+    dao = dao_factory.get('FailedTickerDAO')
+    dao.update(failed_codes)
+
+
 @batch_status(batch_lookup.FN_SNAPSHOT_CONVERT['name'])
 def convert_snapshot(queue: Queue = None):
     """
     FnGuide에서 수집한 FnSnapshot HTML 자료를 변환하여 DB에 저장한다.
-    :param queue: Queue, optional, 작업대상 주식코드를 별도로 지정할 때 사용
+    :param queue: Queue, optional, 처리할 주식코드를 별도로 지정할 때 사용
     :return:
         List[처리된 종목코드]
     """
@@ -29,6 +34,7 @@ def convert_snapshot(queue: Queue = None):
         return
 
     processed_codes = []
+    failed_codes = []
     summary_dao: StockSummaryDAO = dao_factory.get('StockSummaryDAO')
     statement_dao: FnStatementAbbrDAO = dao_factory.get('FnStatementAbbrDAO')
     parser = FnSnapshotParser()
@@ -45,18 +51,22 @@ def convert_snapshot(queue: Queue = None):
             statement_dao.update(dto.annual_abbreviations)
             statement_dao.update(dto.quarter_abbreviations)
 
-            queue_factory.delete(job_name, stock_code)
+            queue_factory.remove_queue_item(job_name, stock_code)
             processed_codes.append(stock_code)
         except FileNotFoundError as err:
             logger.error(str(err))
+            failed_codes.append({'date': start, 'job_name': job_name, 'stock_code': stock_code, 'cause': 'FileNotFound'})
         except StatementParseError as err:
             logger.error(str(err))
+            failed_codes.append({'date': start, 'job_name': job_name, 'stock_code': stock_code, 'cause': str(err)})
 
         elapsed = datetime.now() - start
         logger.debug(f'..Snapshot converted - {stock_code}, elapsed:{elapsed}')
 
     if not queue.empty():
         logger.info(f'There are unresolved financial snapshots: {queue.qsize()}, check error.log')
+        save_failed_ticker(failed_codes)
+        queue_factory.remove_batch_queue(job_name)
 
     return processed_codes
 
@@ -65,7 +75,7 @@ def convert_snapshot(queue: Queue = None):
 def convert_statement(queue: Queue = None):
     """
     FnGuide에서 수집한 재무제표 HTML 자료를 변환하여 DB에 저장한다.
-    :param queue: Queue, optional, 작업대상 주식코드를 별도로 지정할 때 사용
+    :param queue: Queue, optional, 처리할 주식코드를 별도로 지정할 때 사용
     :return:
         List[처리된 종목코드]
     """
@@ -77,6 +87,7 @@ def convert_statement(queue: Queue = None):
         return
 
     processed_codes = []
+    failed_codes = []
     statement_dao: FnStatementDAO = dao_factory.get('FnStatementDAO')
     parser = FnStatementParser()
 
@@ -91,18 +102,23 @@ def convert_statement(queue: Queue = None):
             statement_dao.update(dto.annual_statements)
             statement_dao.update(dto.quarter_statements)
 
-            queue_factory.delete(job_name, stock_code)
+            queue_factory.remove_queue_item(job_name, stock_code)
             processed_codes.append(stock_code)
         except FileNotFoundError as err:
             logger.error(str(err))
+            failed_codes.append(
+                {'date': start, 'job_name': job_name, 'stock_code': stock_code, 'cause': 'FileNotFound'})
         except StatementParseError as err:
             logger.error(str(err))
+            failed_codes.append({'date': start, 'job_name': job_name, 'stock_code': stock_code, 'cause': str(err)})
 
         elapsed = datetime.now() - start
         logger.debug(f'..Statement converted - {stock_code}, elapsed:{elapsed}')
 
     if not queue.empty():
         logger.info(f'There are unresolved financial statements: {queue.qsize()}, check error.log')
+        save_failed_ticker(failed_codes)
+        queue_factory.remove_batch_queue(job_name)
 
     return processed_codes
 
